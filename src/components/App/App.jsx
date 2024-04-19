da58a18d5f780f875c3cee4958b632c492a5e78a
Dmytro Zhuk <140021292+dm-zhuk@users.noreply.github.com>
2024-04-20 00:07:02 +0300

    update App.jsx with useState, useEffect hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { Container, Title, SubTitle } from './index';
 import ContactForm from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  handleAddContact = (name, number) => {
-    const { contacts } = this.state;
+  const handleAddContact = (name, number) => {
     const isExist = contacts.some(
       contact => contact.name.toLowerCase() === name.toLowerCase()
     );
@@ -21,59 +18,42 @@ class App extends Component {
       return;
     }
     const newContact = { id: nanoid(), name, number };
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    setContacts(prevContacts => [...prevContacts, newContact]);
   };
 
-  handleDeleteContact = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== id),
-    }));
+  const handleDeleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
   };
 
-  handleFilterChange = evt => {
-    this.setState({ filter: evt.target.value });
+  const handleFilterChange = evt => {
+    setFilter(evt.target.value);
   };
 
-  //  React components Lifecycle Methods -didMount,-didUpdate  //
-  componentDidMount() {
+  //  `useEffect` - instead od prev React components Lifecycle Methods -didMount,-didUpdate
+  //  `useEffect` hook is used to load the contacts from local storage when the component mounts and save the contacts to local storage whenever the contacts state changes
+  useEffect(() => {
     const localStorageContacts = localStorage.getItem('contacts');
-    if (localStorageContacts && JSON.parse(localStorageContacts).length > 0) {
-      this.setState({ contacts: JSON.parse(localStorageContacts) });
-    }
-  }
+    const parsedContacts = localStorageContacts
+      ? JSON.parse(localStorageContacts)
+      : [];
+    setContacts(parsedContacts);
+  }, []);
 
-  componentDidUpdate(_, prevState) {
-    if (this.state.contacts.length !== prevState.contacts.length) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-    if (prevState.contacts.length > this.state.contacts.length) {
-      console.log('deleted successfully');
-    } else if (prevState.contacts.length < this.state.contacts.length) {
-      console.log('created successfully');
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  render() {
-    const { contacts, filter } = this.state;
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter)
-    );
-
-    return (
-      <Container>
-        <Title>✆ Phonebook ✆</Title>
-        <ContactForm onAddContact={this.handleAddContact} />
-        <SubTitle>Contacts</SubTitle>
-        <Filter value={filter} onChange={this.handleFilterChange} />
-        <ContactList
-          contacts={filteredContacts}
-          onDeleteContact={this.handleDeleteContact}
-        />
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Title>✆ Phonebook ✆</Title>
+      <ContactForm onAddContact={handleAddContact} />
+      <SubTitle>Contacts</SubTitle>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ContactList contacts={contacts} onDeleteContact={handleDeleteContact} />
+    </Container>
+  );
+};
 
 export default App;
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,14 +2,19 @@ import React, { useState } from 'react';
 import { Form, Label, Input, SubmitButton } from './index';
 
 const ContactForm = ({ onAddContact }) => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+  const [contact, setContact] = useState({ name: '', number: '' });
 
+  //  ⨐ uses the contact state to pass the name and number values to the onAddContact ⨐
   const handleSubmit = evt => {
     evt.preventDefault();
-    onAddContact(name, number);
-    setName('');
-    setNumber('');
+    onAddContact(contact.name, contact.number);
+    setContact({ name: '', number: '' });
+  };
+
+  //  to update the respective property when the input values change
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+    setContact(prevContact => ({ ...prevContact, [name]: value }));
   };
 
   return (
@@ -20,8 +25,8 @@ const ContactForm = ({ onAddContact }) => {
         name="name"
         pattern="^[a-zєіїA-Zа-яА-ЯІ]+(([' \-][a-zA-Zа-яєіїА-ЯІ ])?[a-zA-Zа-яєіїА-ЯІ]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-        value={name}
-        onChange={evt => setName(evt.target.value)}
+        value={contact.name}
+        onChange={handleChange} //  now consolidating the state into a single object and using the same `handleChange`⨐ for both inputs
         required
       />
       <Label>Number:</Label>
@@ -30,8 +35,8 @@ const ContactForm = ({ onAddContact }) => {
         name="number"
         pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses, and can start with +"
-        value={number}
-        onChange={evt => setNumber(evt.target.value)}
+        value={contact.number}
+        onChange={handleChange}
         required
       />
       <SubmitButton type="submit">Add Contact</SubmitButton>
